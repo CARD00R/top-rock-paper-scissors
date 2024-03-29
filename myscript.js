673857516b8d5083b26388882d1bdce0278a6536
CARD00R <39516150+CARD00R@users.noreply.github.com>
2024-03-29 19:41:38 +0400

    UI Update Stage 1

--- myscript.js
+++ myscript.js
@@ -1,6 +1,7 @@
 // Stored Variables
 let playerScore = 0;
 let computerScore = 0;
+let resultText = "Result: ";
 
 // Get a random choice 
 function getRandomChoice(){
@@ -10,18 +11,25 @@ function getRandomChoice(){
     return choice[numericalChoice];
 
 }
-// Print the scores of the player and computer
-function printScore(){
-    console.log("Player Score: " + playerScore);
-    console.log("Computer Score: " + computerScore);
+// Update the scores of the player and computer
+function UpdateScore(aResult){
+    
+    let resultEl = document.getElementById('result');
+    resultEl.textContent = "RESULT: " + aResult;
+
+    let playerScoreEl = document.getElementsByClassName('scoreContainer')[0].childNodes[1];
+    playerScoreEl.textContent = "Player Score: " + playerScore;
+
+    let computerScoreEl = document.getElementsByClassName('scoreContainer')[0].childNodes[3];
+    computerScoreEl.textContent = "Computer Score: " + computerScore;
 }
 
 // Play 1 full round of Rock paper scissors
 function playRound(playerSelection, computerSelection){
-    
+    console.log(computerSelection)
     // If player and computer have the same answer
     if(playerSelection === computerSelection){
-        console.log("Its a draw!");
+        UpdateScore("A DRAW!");
         return;
     }
     // If player chooses rock
@@ -29,15 +37,15 @@ function playRound(playerSelection, computerSelection){
        
         // and computer chooses scissors
         if(computerSelection === "scissors"){
-            console.log("You Win! Rock beats Scissors");
             playerScore++;
+            UpdateScore("PLAYER Wins! Rock beats Scissors");
             return;
         }
 
         // and computer chooses paper
         if(computerSelection === "paper"){
-            console.log("You lose! Paper beats Rock");
             computerScore++;
+            UpdateScore("COMPUTER Wins! Paper beats Rock");
             return;
         }
     }
@@ -45,15 +53,15 @@ function playRound(playerSelection, computerSelection){
        
         // and computer chooses scissors
         if(computerSelection === "rock"){
-            console.log("You Win! Paper beats Rock");
             playerScore++;
+            UpdateScore("PLAYER Wins! Paper beats Rock");
             return;
         }
 
         // and computer chooses paper
         if(computerSelection === "scissors"){
-            console.log("You lose! Scissors beats paper");
             computerScore++;
+            UpdateScore("COMPUTER Wins! Scissors beats paper");
             return;
         }
     }
@@ -61,48 +69,82 @@ function playRound(playerSelection, computerSelection){
        
         // and computer chooses scissors
         if(computerSelection === "paper"){
-            console.log("You Win! Scissors beats Paper");
             playerScore++;
+            UpdateScore("PLAYER Wins! Scissors beats Paper");
             return;
         }
 
         // and computer chooses paper
         if(computerSelection === "rock"){
-            console.log("You lose! Rock beats scissors");
             computerScore++;            
+            UpdateScore("COMPUTER Wins! Rock beats scissors");
             return;
         }
     }
     else {
-        console.log("Idk what you inputted, but I dont get");
+        console.log("Bad Input");
     }
 
 }
 
-// Player X amount of rounds of Rock Paper Scissors
-function playGame(rounds){
 
-    for(let i = 0; i < rounds; i++){
 
-        if(playerScore === 3 || computerScore === 3){
-            if(playerScore>computerScore){
-                console.log("Player wins!")
-            }
-            else {
-                console.log("Computer wins!")
-            }
-            return; 
-        } 
+let btnContainer = document.getElementsByClassName('playerInput')[0];
+if(btnContainer){
+    btnContainer.addEventListener('click', (Event) =>{
+        let target = Event.target;
 
-        console.log("ROUND: " + (i+1)); 
-        playerSelection = window.prompt("Choose Rock, Paper or Scissors").toLowerCase();
-        computerSelection = getRandomChoice();
-        console.log("Player: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-        playRound(playerSelection, computerSelection);
-        printScore();
-        console.log(" ");
-    }
+        switch(target.id){
+            case 'rock':
+                playRound('rock', getRandomChoice())
+                break;
+            case 'paper':
+                playRound('paper', getRandomChoice())
+                break;
+            case 'scissors':
+                playRound('scissors', getRandomChoice())
+                break;
+            default:
+                console.log("no matching ID found")
+        }
+        
+    });
 }
 
-console.log(playGame(10));
+UpdateScore(' ');
+
+
+
+// // Player X amount of rounds of Rock Paper Scissors
+// function playGame(rounds){
+
+//     for(let i = 0; i < rounds; i++){
+
+//         if(playerScore === 3 || computerScore === 3){
+//             if(playerScore>computerScore){
+//                 console.log("Player wins!")
+//             }
+//             else {
+//                 console.log("Computer wins!")
+//             }
+//             return; 
+//         } 
+
+//         console.log("ROUND: " + (i+1)); 
+//         playerSelection = window.prompt("Choose Rock, Paper or Scissors").toLowerCase();
+//         computerSelection = getRandomChoice();
+//         console.log("Player: " + playerSelection);
+//         console.log("Computer: " + computerSelection);
+//         playRound(playerSelection, computerSelection);
+//         printScore();
+//         console.log(" ");
+//     }
+// }
+
+
+
+
+
+
+
+
